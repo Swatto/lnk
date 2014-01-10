2f58a9cd1fc54b7df5620f860795a8d47909a7c5
Gaël Gillard <gillardgael@gmail.com>
2014-01-10 23:17:12 +0100

    Remove the line from NVM install file

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ LNK_DIR="$HOME/.lnk"
 
 if ! hash git 2>/dev/null; then
   echo >&2 "You need to install git - visit http://git-scm.com/downloads"
-  echo >&2 "or, use install-gitless.sh instead."
   exit 1
 fi
 
